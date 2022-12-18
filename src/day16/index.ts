cedce37e4563cdd2aa435f72c92b99ea96cde50b
Julius Schmidt <newceptiondev@googlemail.com>
2022-12-18 17:35:57 +0100

    added day 16 task 2

--- src/day16/index.ts
+++ src/day16/index.ts
@@ -13,14 +13,8 @@ interface Valve {
 
 interface Path {
   currentPosition: string
-  elephantPosition?: string
-  nextMoveOwn?: number
-  nextMoveElephant?: number
-  currentFlowRate: number
-  pressureReleased: number
+  pressureReleasedAtEnd: number
   openedValves: string[]
-  targetOwn?: string
-  targetElephant?: string
   timeGone: number
 }
 
@@ -40,9 +34,7 @@ const distanceToValve = (
   current: string,
   target: string,
   wayMap: Map<string, Map<string, string[]>>
-): number => {
-  return wayMap.get(current).get(target).length
-}
+): number => wayMap.get(current).get(target).length
 
 const buildWayMap = (valves: Valve[]): Map<string, Map<string, string[]>> => {
   const wayMap = new Map<string, Map<string, string[]>>()
@@ -79,75 +71,83 @@ const buildWayMap = (valves: Valve[]): Map<string, Map<string, string[]>> => {
   return wayMap
 }
 
+const calculateMaxPressureValveMap = (valves: Valve[], wayMap: Map<string, Map<string, string[]>>, maxTime: number): Map<string[], number> => {
+  const maxPressureForValves = new Map<string[], number>()
+
+  const startValve = {
+    currentPosition: "AA",
+    pressureReleasedAtEnd: 0,
+    openedValves: [],
+    timeGone: 0,
+  }
+
+  let paths = [startValve]
+
+  while(paths.length > 0) {
+    const nextPaths: Path[] = []
+
+    paths.forEach(path => {
+      const possible = valves.filter(valve => !path.openedValves.includes(valve.identifier) && distanceToValve(path.currentPosition, valve.identifier, wayMap) + 2 < (maxTime - path.timeGone - 1))
+
+      possible.forEach(possiblePath => {
+        const distance = distanceToValve(path.currentPosition, possiblePath.identifier, wayMap)
+        const updatedTime = path.timeGone + distance + 2
+        const newEntry = {
+          currentPosition: possiblePath.identifier,
+          openedValves: [...path.openedValves, possiblePath.identifier],
+          timeGone: updatedTime,
+          pressureReleasedAtEnd: path.pressureReleasedAtEnd + (maxTime - updatedTime) * possiblePath.flowRate
+        }
+        nextPaths.push(newEntry)
+
+        const maxPressureEntryForValves = Array.from(maxPressureForValves.keys()).find(set => set.length === newEntry.openedValves.length && newEntry.openedValves.every(valve => set.includes(valve)))
+        if(!maxPressureEntryForValves) {
+          maxPressureForValves.set(newEntry.openedValves, newEntry.pressureReleasedAtEnd)
+        } else if(maxPressureForValves.get(maxPressureEntryForValves) < newEntry.pressureReleasedAtEnd) {
+          maxPressureForValves.set(maxPressureEntryForValves, newEntry.pressureReleasedAtEnd)
+        }
+      })
+    })
+    paths = nextPaths
+  }
+
+  return maxPressureForValves
+}
+
 const goA = (input) => {
   const lines = splitToLines(input)
   let valves = lines.map(parseValve)
 
-  const current = {
-    currentPosition: "AA",
-    currentFlowRate: 0,
-    pressureReleased: 0,
-    openedValves: ["AA"],
-    timeGone: 0,
-  }
-
   const wayMap = buildWayMap(valves)
-
   valves = valves.filter((valve) => valve.flowRate !== 0)
+  const maxPressureForValves = calculateMaxPressureValveMap(valves, wayMap, 30)
 
-  let paths = [current]
-
-  let maxPressure = 0
-  while (paths.length > 0) {
-    const nextPaths: Path[] = []
-
-    paths.forEach((path) => {
-      const possible = valves.filter((valve) => !path.openedValves.includes(valve.identifier))
-
-      if (possible.length === 0) {
-        if (path.timeGone < 30) {
-          const timeLeft = 30 - path.timeGone
-          path.pressureReleased += path.currentFlowRate * timeLeft
-        }
-
-        if (path.pressureReleased > maxPressure) {
-          maxPressure = path.pressureReleased
-        }
-      }
-
-      possible.forEach((possiblePath) => {
-        const steps = distanceToValve(path.currentPosition, possiblePath.identifier, wayMap) + 2
-
-        if (path.timeGone + steps > 30) {
-          let endPressure = path.pressureReleased
-          if (path.timeGone < 30) {
-            const timeLeft = 30 - path.timeGone
-            endPressure += path.currentFlowRate * timeLeft
-          }
-
-          if (endPressure > maxPressure) {
-            maxPressure = endPressure
-          }
-        } else {
-          nextPaths.push({
-            timeGone: path.timeGone + steps,
-            pressureReleased: path.pressureReleased + steps * path.currentFlowRate,
-            currentFlowRate: path.currentFlowRate + possiblePath.flowRate,
-            currentPosition: possiblePath.identifier,
-            openedValves: [...path.openedValves, possiblePath.identifier],
-          })
-        }
-      })
-    })
-
-    paths = nextPaths
-  }
-
-  return maxPressure
+  return Array.from(maxPressureForValves.values()).sort((a, b) => a - b).pop()
 }
 
 const goB = (input) => {
-  return 0
+  const lines = splitToLines(input)
+  let valves = lines.map(parseValve)
+
+  const wayMap = buildWayMap(valves)
+  valves = valves.filter((valve) => valve.flowRate !== 0)
+  const maxPressureForValves = calculateMaxPressureValveMap(valves, wayMap, 26)
+  const keys = Array.from(maxPressureForValves.keys())
+
+  let maxReleasablePressure = 0
+
+  for(let i = 0; i < keys.length; i++) {
+    for(let j = i + 1; j < keys.length; j++) {
+      if(keys[i].every(valve => !keys[j].includes(valve))) {
+        const releasablePressure = maxPressureForValves.get(keys[i]) + maxPressureForValves.get(keys[j])
+        if(maxReleasablePressure < releasablePressure) {
+          maxReleasablePressure = releasablePressure
+        }
+      }
+    }
+  }
+
+  return maxReleasablePressure
 }
 
 /* Tests */
